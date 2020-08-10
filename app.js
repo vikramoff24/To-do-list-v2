f4c997835bf31e88c127a4c0254f8568bccf5761
vikramoff24 <vikram.senthil@outlook.com>
2020-08-10 23:42:33 +0530

    Heroku Update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true,useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-vikram:vikram2000@cluster0.dbmew.mongodb.net/todolistDB",{useNewUrlParser:true,useUnifiedTopology: true});
 const itemsSchema={
   name:String
 };
